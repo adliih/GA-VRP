e8648bb2a3ba40628edef2517cee19990ce234e7
Adli Ihsan Hariadi <adliih@ymail.com>
2016-02-28 14:30:59 +0700

    bug fixes not sorted fitness val in next generation

--- choose_next_gen.m
+++ choose_next_gen.m
@@ -6,4 +6,4 @@ function [next_population next_fitness_val] = choose_next_gen(population, fitnes
 	population = population(sorted_ind,:);
 	offsprings = offsprings(sorted_ind_offsprings, :);
 	next_population = [population(1:2,:);offsprings(3:end,:)];
-	next_fitness_val = [sorted_val(1:2,:);fitness_val_offsprings(3:end,:)];
+	next_fitness_val = [sorted_val(1:2,:);sorted_val_offsprings(3:end,:)];
